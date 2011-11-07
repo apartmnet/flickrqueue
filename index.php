75cebc193ff0d24b6bcf5ef66eb0f329cc77c251
Erin Sparling <erin@everyplace.net>
2011-11-06 22:09:43 -0500

    Updated styles

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <html>
 	<head>
 		<title>flickrQueue</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
 		<link rel="stylesheet" href="css/styles.css" />
 		<script type="text/javascript" src="http://use.typekit.com/etv3fss.js"></script>
 		<script type="text/javascript">try{Typekit.load();}catch(e){}</script>
@@ -13,7 +14,7 @@
 				<h2>automated Flickr photo privacy modification for the masses</h2>
 			</hgroup>
 			<ul>
-				<li>Authorize flickrQueue to access your photos.</li>
+				<li><a href="auth">Authorize</a> flickrQueue to access your photos.</li>
 				<li>Upload photos as your normally would, but mark them as private, and tag them with "flickrqueue".</li>
 				<li>Wait until midnight EST. Every night.</li>
 			</ul>
